9d9cc3ddf92191fa4e930b6af33e24650dbf764e
michal6769 <mike.rutkowski11@gmail.com>
2026-02-10 19:41:08 +0100

    Add dictionary for slang word definitions

---
+++ lekcje.py
@@ -0,0 +1,19 @@
+
+slownik = {
+"CRINGE": "Coś wyjątkowego dziwnego lub wstydliwego",
+"LOL": "odpowiedź na coś zabawnego",
+"ROFL": "odpowiedź na żart",
+"SHEESH": "lekka dezaprobata",
+"CREEPY": "straszny, złowieszczy",
+"AGGRO": "stać się agresywnym/zły",
+"APPLE": "jabłko",
+"CAR": "samochód",
+"CAT": "kot",   
+}
+
+word = input("Wpisz słwo, którego nie rozumiesz(używaj wielkich liter!): ") 
+
+if word in slownik.keys():
+    print(slownik[word])
+else:
+    print("Nie rozumiem tego słowa")
